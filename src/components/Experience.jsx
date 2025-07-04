6c63507797be0d5dfae54a79379470d0ccbbb280
phanisaimunipalli <phanisaimunipalli@gmail.com>
2025-07-04 12:03:41 -0700

    MPSR - Updated Experiences

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,34 +1,13 @@
 import React, { useEffect, useState, useContext } from 'react';
 import { Timeline, TimelineItem } from 'vertical-timeline-component-for-react';
-import { Container } from 'react-bootstrap';
+import { ThemeContext } from 'styled-components';
 import ReactMarkdown from 'react-markdown';
 import PropTypes from 'prop-types';
-import { ThemeContext } from 'styled-components';
 import Fade from 'react-reveal';
 import Header from './Header';
 import endpoints from '../constants/endpoints';
 import FallbackSpinner from './FallbackSpinner';
 import '../css/experience.css';
-
-const styles = {
-  ulStyle: {
-    listStylePosition: 'outside',
-    paddingLeft: 20,
-  },
-  subtitleContainerStyle: {
-    marginTop: 10,
-    marginBottom: 10,
-  },
-  subtitleStyle: {
-    display: 'inline-block',
-  },
-  inlineChild: {
-    display: 'inline-block',
-  },
-  itemStyle: {
-    marginBottom: 10,
-  },
-};
 
 function Experience(props) {
   const theme = useContext(ThemeContext);
@@ -40,7 +19,14 @@ function Experience(props) {
       method: 'GET',
     })
       .then((res) => res.json())
-      .then((res) => setData(res.experiences))
+      .then((res) => {
+        const { workTypes, experiences } = res;
+        const processedData = experiences.map((item) => ({
+          ...item,
+          workType: workTypes[item.workTypeId],
+        }));
+        setData(processedData);
+      })
       .catch((err) => err);
   }, []);
 
@@ -48,59 +34,57 @@ function Experience(props) {
     <>
       <Header title={header} />
 
-      {data
-        ? (
-          <div className="section-content-container">
-            <Container>
-              <Timeline
-                lineColor={theme.timelineLineColor}
-              >
-                {data.map((item) => (
-                  <Fade>
-                    <TimelineItem
-                      key={item.title + item.dateText}
-                      dateText={item.dateText}
-                      dateInnerStyle={{ background: theme.accentColor }}
-                      style={styles.itemStyle}
-                      bodyContainerStyle={{ color: theme.color }}
-                    >
-                      <h2 className="item-title">
-                        {item.title}
-                      </h2>
-                      <div style={styles.subtitleContainerStyle}>
-                        <h4 style={{ ...styles.subtitleStyle, color: theme.accentColor }}>
-                          {item.subtitle}
-                        </h4>
-                        {item.workType && (
-                        <h5 style={styles.inlineChild}>
-                    &nbsp;·
-                          {' '}
-                          {item.workType}
-                        </h5>
-                        )}
-                      </div>
-                      <ul style={styles.ulStyle}>
-                        {item.workDescription.map((point) => (
-                          <div key={point}>
-                            <li>
-                              <ReactMarkdown
-                                children={point}
-                                components={{
-                                  p: 'span',
-                                }}
-                              />
-                            </li>
-                            <br />
-                          </div>
-                        ))}
-                      </ul>
-                    </TimelineItem>
-                  </Fade>
-                ))}
-              </Timeline>
-            </Container>
-          </div>
-        ) : <FallbackSpinner /> }
+      {data ? (
+        <div className="section-content-container">
+          <Timeline lineColor={theme.timelineLineColor}>
+            {data.map((item) => (
+              <Fade key={item.title}>
+                <TimelineItem
+                  key={item.title + item.dateText}
+                  dateText={item.dateText}
+                  dateInnerStyle={{ background: theme.accentColor, color: 'white' }}
+                  bodyContainerStyle={{ padding: '0', background: 'transparent', boxShadow: 'none' }}
+                >
+                  {/* Custom Card Content */}
+                  <div className="experience-card-content">
+                    {/* Title */}
+                    <h3 className="card-title">{item.title}</h3>
+
+                    {/* Subtitle and Work Type */}
+                    <div className="card-subtitle-container">
+                      <h4 className="card-subtitle">{item.subtitle}</h4>
+                      {item.workType && (
+                        <span className="card-work-type">· {item.workType}</span>
+                      )}
+                    </div>
+
+                    {/* Description */}
+                    <p className="card-description">
+                      <ReactMarkdown
+                        children={item.workDescription}
+                        components={{
+                          p: 'span',
+                        }}
+                      />
+                    </p>
+
+                    {/* Key Skills */}
+                    <div className="card-skills-container">
+                      {item.keySkills.map((skill) => (
+                        <span key={skill} className="skill-tag">
+                          {skill}
+                        </span>
+                      ))}
+                    </div>
+                  </div>
+                </TimelineItem>
+              </Fade>
+            ))}
+          </Timeline>
+        </div>
+      ) : (
+        <FallbackSpinner />
+      )}
     </>
   );
 }
